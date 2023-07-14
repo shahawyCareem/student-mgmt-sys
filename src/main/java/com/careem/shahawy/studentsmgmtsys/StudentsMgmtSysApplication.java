b3500ef8de978bd887631e0cda1569a0a216aae7
ahmed.elshahawy <ahmed.elshahawy@careem.com>
2023-07-14 15:59:32 +0300

    Project initialization

---
+++ src/main/java/com/careem/shahawy/studentsmgmtsys/StudentsMgmtSysApplication.java
@@ -0,0 +1,13 @@
+package com.careem.shahawy.studentsmgmtsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentsMgmtSysApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentsMgmtSysApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/careem/shahawy/studentsmgmtsys/StudentsMgmtSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careem.shahawy.studentsmgmtsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsMgmtSysApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
